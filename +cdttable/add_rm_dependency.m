aa303f77d1c12eae1e012787bb8e06d66b91da4d
Yimeng Zhang <zym1010@gmail.com>
2016-02-28 19:49:11 -0500

    fix dependency solving when the code is already in static path.

--- +cdttable/add_rm_dependency.m
+++ +cdttable/add_rm_dependency.m
@@ -5,6 +5,13 @@ function add_rm_dependency( choice )
 import cdttable.root_dir
 javalibPath = fullfile(root_dir(), 'json-schema-validator-2.2.6-lib.jar');
 currentJavaClassPath = javaclasspath('-dynamic');
+currentJavaClassPathStatic = javaclasspath('-static');
+
+% it's already in the static path.
+if any(strcmp(currentJavaClassPathStatic,javalibPath))
+    return
+end
+
 if isequal(choice, 'add')
     assert(all(~strcmp(currentJavaClassPath,javalibPath)))
     javaaddpath(javalibPath);
